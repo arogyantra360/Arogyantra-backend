e67ea7a89655fc42658ae82e66cdf9e586de5f9c
Arogyantra <arogyantra360@gmail.com>
2025-10-08 00:38:49 +0530

    fix: handle OPTIONS method for Vercel CORS

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export default {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "arogyantra-XXXX.firebaseapp.com",
+  databaseURL: "https://arogyantra-XXXX-default-rtdb.firebaseio.com",
+  projectId: "arogyantra-XXXX",
+  storageBucket: "arogyantra-XXXX.appspot.com",
+  messagingSenderId: "XXXXXXX",
+  appId: "1:XXXXXXX:web:XXXXXXX"
+};
